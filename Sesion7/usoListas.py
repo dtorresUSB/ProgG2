b2be8ea890646ecba7664f1312599d4bb8ce7b9c
dtorresUSB <dtorres@usbbog.edu.co>
2024-09-04 10:54:21 -0500

    sesion_7

--- Sesion7/usoListas.py
+++ Sesion7/usoListas.py
@@ -13,6 +13,17 @@ def borrar(lista):
     lista.clear()
     return lista
 
+def remover(lista):
+    num=int(input('Que numero desea remover: '))
+    lista.remove(num)
+    return lista
+
+def indice(lista):
+    num=int(input('Que numero busca: '))
+    pos=lista.index(num)
+    print(f'El número {num} se encuentra en la posicion {pos}')
+    return lista
+
 def main():
     lista=[2,54,15]
 
@@ -20,15 +31,14 @@ def main():
         print('\nMenú de uso de listas: \n'
           '1. Agregar un elemento a la lista.\n'
           '2. Insertar un elemento dentro de la lista\n'
-          '3. Borrar por completo la lista\n')
-        opc=input('Seleccione una de las opciones: ')
+          '3. Borrar por completo la lista\n'
+          '4. Remover un elemento de la lista\n'
+          '5. Busqueda del indice de un número\n')
+        opc=int(input('Seleccione una de las opciones: '))
 
-        if opc=='1':
-            lista=agregar(lista)
-        elif opc=='2':
-            lista=insertar(lista)
-        elif opc=='3':
-            lista=borrar(lista)
+        menu=[agregar,insertar,borrar,remover,
+              indice]
+        lista=menu[opc-1](lista)
         print(lista)
 
         if opc=='salir':
