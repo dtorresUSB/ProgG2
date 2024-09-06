fe231468423d61b67709660cef1bf3d861a72cc8
dtorresUSB <dtorres@usbbog.edu.co>
2024-09-06 11:06:42 -0500

    sesion 7 y 8

--- Sesion7/usoListas.py
+++ Sesion7/usoListas.py
@@ -24,6 +24,11 @@ def indice(lista):
     print(f'El número {num} se encuentra en la posicion {pos}')
     return lista
 
+def minimo(lista):
+    valor=min(lista)
+    print(f'El valor minimo es {valor}')
+    return lista
+
 def main():
     lista=[2,54,15]
 
@@ -33,11 +38,12 @@ def main():
           '2. Insertar un elemento dentro de la lista\n'
           '3. Borrar por completo la lista\n'
           '4. Remover un elemento de la lista\n'
-          '5. Busqueda del indice de un número\n')
+          '5. Busqueda del indice de un número\n'
+          '6. Encontrar el valor minimo de una lista')
         opc=int(input('Seleccione una de las opciones: '))
 
         menu=[agregar,insertar,borrar,remover,
-              indice]
+              indice, minimo]
         lista=menu[opc-1](lista)
         print(lista)
 
