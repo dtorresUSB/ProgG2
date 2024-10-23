4e78d5be44e57951e239d3be334c693153955afa
dtorresUSB <dtorres@usbbog.edu.co>
2024-10-23 12:08:45 -0500

    interfaz grafica

---
+++ Sesion14y15/app.py
@@ -0,0 +1,40 @@
+import PyQt5.QtWidgets as pyqt
+from PyQt5 import uic
+import sys
+
+class Principal(pyqt.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initGUI()
+    
+    def initGUI(self):
+        uic.loadUi('calculadora.ui',self)
+        self.show()
+
+        self.boton_calcular.clicked.connect(lambda :
+                                            self.operaciones
+                                            (int(self.numero1.text()),
+                                             int(self.numero2.text())))
+
+    def operaciones(self,num1,num2):
+        if self.suma.isChecked():
+            txt=str(num1+num2)
+        elif self.resta.isChecked():
+            txt=str(num1-num2)
+        elif self.producto.isChecked():
+            txt=str(num1*num2)
+        elif self.division.isChecked():
+            txt=str(round((num1/num2),4))
+        else:
+            txt='Error'
+        self.resultado.setText(txt)
+
+   
+
+def main():
+    app = pyqt.QApplication([])
+    window = Principal()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
